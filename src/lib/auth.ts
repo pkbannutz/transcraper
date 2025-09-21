935fee4db89528e7be2560a2111b5b8d98e73f82
pkbannutz <99034039+pkbannutz@users.noreply.github.com>
2025-09-21 19:32:06 +0200

    Fix: Add missing NextAuth import

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,5 @@
 import type { NextAuthConfig } from "next-auth"
+import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import GoogleProvider from "next-auth/providers/google"
 import GitHubProvider from "next-auth/providers/github"
